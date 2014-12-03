81fbb3f69a77a81d6c214b491a43577b0d33ce94
Robert Fisher <me@robfisher.us>
2014-12-03 11:05:54 -0500

    Added Map w/ SLOW drawing of markers

---
+++ client/lib/google_maps.js
@@ -0,0 +1,85 @@
+gmaps = {
+  map: null,
+
+  bounds: {},
+
+  markers: [],
+
+  latLngs: [],
+
+  markerData: [],
+
+  addMarker: function(marker) {
+    var gLatLng = new google.maps.LatLng(marker.lat, marker.lng);
+    var gMarker = new google.maps.Marker({
+      position: gLatLng,
+      map: this.map,
+      title: marker.title,
+      //animation: google.maps.Animation.DROP,
+      icon:'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+    });
+
+    this.latLngs.push(gLatLng);
+    this.markers.push(gMarker);
+    this.markerData.push(marker);
+    return gMarker;
+  },
+
+  clearMarkers: function() {
+    _.each(this.markers, function(marker) {
+      marker.setMap(null);
+    });
+
+    this.markers = [];
+    this.latLngs = [];
+    this.markerData = [];
+  },
+
+  markerExists: function(key, val) {
+    _.each(this.markerData, function(marker) {
+      if (marker[key] === val) {
+        return true;
+      }
+    });
+
+    return false;
+  },
+
+  initialize: function() {
+    var mapOptions = {
+      zoom: 16,
+      minZoom: 12,
+      center: new google.maps.LatLng(40.044171, -76.313411),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(
+      document.getElementById('map-canvas'),
+      mapOptions
+    );
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var location = new google.maps.LatLng(position.coords.latitude,
+          position.coords.longitude);
+
+          this.currLocation = new google.maps.Marker({
+            position: location,
+            map: map,
+            draggable: false,
+            animation: google.maps.Animation.DROP,
+            title: 'Your Location'
+          });
+
+          map.setCenter(location);
+        }, function() {
+          toastr.error('Geo-Location Error');
+        });
+      } else {
+        toastr.error('Your browser doesn\'t support geolocation');
+      }
+
+      this.map = map;
+      Session.set('map', true);
+  }
+}
---
+++ client/templates/map/map.js
@@ -0,0 +1,30 @@
+Template.map.rendered = function() {
+  if (!Session.get('map')) {
+    gmaps.initialize();
+  }
+
+  Deps.autorun(function() {
+    var locations = Locations.find().fetch();
+
+    if (locations) {
+      _.each(locations, function(location) {
+        if(location) {
+          if (!gmaps.markerExists('id', location._id)) {
+              var marker = {
+                id: location._id,
+                lat: location.lat,
+                lng: location.lng,
+                title: location.name
+              };
+
+              gmaps.addMarker(marker);
+          }
+        }
+      });
+    }
+  });
+}
+
+Template.map.destroyed = function() {
+  Session.set('map', false);
+}
